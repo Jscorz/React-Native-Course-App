972ef8dd44e00745537405a859fb5a9e74527064
Justin Scorzafava <jkscorzafava@gmail.com>
2022-11-20 18:20:38 -0700

    added basic layout, and stylesheet with basic styles

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View>
+				<Text style={styles.dummyText}>Hello World!</Text>
+			</View>
+			<Text style={styles.dummyText}>Another piece of text!</Text>
+			<Button title='Tap me!' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	dummyText: {
+		margin: 16,
+		padding: 16,
+		borderWidth: 2,
+		borderColor: "red",
+	},
 });
