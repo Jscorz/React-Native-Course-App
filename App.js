e863d1fb3b8a856725fafd0cdb6dbd6d1383fc5e
Justin Scorzafava <jkscorzafava@gmail.com>
2022-11-25 19:26:36 -0700

    imported ScrollView component for list of goals

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from "react-native";
 
 export default function App() {
 	const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -24,11 +24,13 @@ export default function App() {
 				<Button title='Add Goal' onPress={addGoalHandler} />
 			</View>
 			<View style={styles.goalsContainer}>
-				{courseGoals.map((goal) => (
-					<View style={styles.goalItem} key={goal}>
-						<Text style={styles.goalText}>{goal}</Text>
-					</View>
-				))}
+				<ScrollView>
+					{courseGoals.map((goal) => (
+						<View style={styles.goalItem} key={goal}>
+							<Text style={styles.goalText}>{goal}</Text>
+						</View>
+					))}
+				</ScrollView>
 			</View>
 		</View>
 	);
